5283739818ad8023fd29652d102ecee41e522f20
Juan Gerardo <jmendez1025@cue.edu.co>
2025-10-06 15:29:05 -0500

    initial clean architecture setup

---
+++ applications/app-service/src/main/java/cue/edu/co/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package cue.edu.co.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "cue.edu.co.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
